b1668f5f675d930d1267003a302995d8486cf7c6
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-15 10:01:50 +0530

    Add files via upload
    Day 14: Methods in Java

---
+++ NumberMethod14.java
@@ -0,0 +1,10 @@
+public class NumberMethod14{
+    static void Number(int num){
+        System.out.println(num);
+    }
+    public static void main(String[] args){
+        Number(10);
+        Number(20);
+        Number(30);
+    }
+}
