67f67d046ab5545f8d2b2a46f73d276ecd069499
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-15 10:00:32 +0530

    Add files via upload
    Day 14: Methods in Java

---
+++ HelloMethod14.java
@@ -0,0 +1,8 @@
+public class HelloMethod14{
+    static void SayHello(){
+        System.out.println("Hello, I am Learning Java.");
+    }
+    public static void main(String[] args) {
+        SayHello();
+    }
+}
