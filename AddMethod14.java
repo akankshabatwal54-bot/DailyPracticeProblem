83a2f21fce5ab7f0e4ce4b31f3cf3834058f12ce
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-15 09:59:05 +0530

    Add files via upload
    Day 14: Methods in Java

---
+++ AddMethod14.java
@@ -0,0 +1,10 @@
+public class AddMethod14{
+    static void Add(int a, int b){
+        int sum = a + b;
+        System.out.println("Sum: " + sum );
+    }
+    public static void main(String[] args) {
+        Add(10,30);
+        Add(125,250);
+        Add(50,60);    }
+}
