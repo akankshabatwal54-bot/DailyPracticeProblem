5c9b19e080f46f0e06be253d51425b2c6cf08247
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-16 10:44:13 +0530

    Day 15 Q2: Access First and Last Array Element
    Practiced accessing the first and last elements of an array using array indexing and length.

---
+++ FirstLast15.java
@@ -0,0 +1,9 @@
+public class FirstLast15{
+    public static void main(String[] args){
+        int[] numbers = {5, 10, 15, 20, 25};
+        
+            System.out.println(numbers[0]);
+            System.out.println(numbers[numbers.length-1]);
+        
+    }
+}
