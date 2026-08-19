24f8bb5f80fa1e6c1442b4f499c3081c74205d27
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-19 21:59:13 +0530

    Day 18 Q1: Create Greeting Method
    Practiced creating and calling a simple method in Java.

---
+++ Method18.java
@@ -0,0 +1,8 @@
+public class Method18{
+    static void greet(){
+        System.out.println("Hello, Welcome to Java..");
+    }
+    public static void main(String[] args) {
+        greet();
+    }
+}
