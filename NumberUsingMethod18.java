32f2e1d2cd4aab28674e349c4e34b1728204ea60
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-19 22:02:01 +0530

    Day 18 Q3: Print Numbers Using Method
    Practiced using a method with a for loop to print numbers from 1 to 5.

---
+++ NumberUsingMethod18.java
@@ -0,0 +1,12 @@
+public class NumberUsingMethod18{
+    static void printNumber(){
+        int[] numbers = {1, 2, 3, 4, 5};
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        } 
+    }
+    public static void main(String[] args) {
+        printNumber();
+    }
+}
+    
