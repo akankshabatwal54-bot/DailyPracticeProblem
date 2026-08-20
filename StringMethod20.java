0b19ba2d0cc1db439bb994a0c0d0032d12c072bd
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-21 02:16:20 +0530

    Day 20 Q1: Return Greeting String Using Method
    Practiced returning a String from a method using a parameter.

---
+++ StringMethod20.java
@@ -0,0 +1,9 @@
+public class StringMethod20{
+    static String greet(String name){
+        return "Hello" + name;
+    }
+    public static void main(String[] args) {
+        String result = greet("Akanksha");
+        System.out.println(result);
+    }
+}
